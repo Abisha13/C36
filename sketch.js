66b285a3415683dc6f535176b7a6dedd58859d31
Abisha13 <75430616+Abisha13@users.noreply.github.com>
2021-03-18 18:00:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var gameState=0;
+var playerCount = 0;
+var backgroundIMG;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+}
+function draw(){
+  
+}
